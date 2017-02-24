36e2cd7f8f7857d251b3cf598f5d25cdaffc14dc
SnowGushiGit <snow.akogi.pgel@gmail.com>
2017-02-25 06:35:01 +0900

    image process

--- code/docker/python/search_demo.py
+++ code/docker/python/search_demo.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from __future__ import unicode_literals
+
+import cherrypy, os, urllib, picklefrom numpy import *
+import imagesearch
+
+
+class SearchDemo:
+
+    def __init__(self):
+        f = open("webimlist.txt")
+        self.imlist = f.readlines()
+        f.close()
+        self.nbr_images = len(self.imlist)
